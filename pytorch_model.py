0efe19347260c80aa2cc6094fb7cbc622f6fdf5e
Rakshith Ram C.A. <116709142+Rakshith-Ram@users.noreply.github.com>
2024-10-03 16:35:16 +0530

    Add files via upload

---
+++ pytorch_model.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+
+# Define the model architecture
+class AutonomousDrivingModel(nn.Module):
+    def __init__(self):
+        super(AutonomousDrivingModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 24, kernel_size=5, stride=2)
+        self.conv2 = nn.Conv2d(24, 36, kernel_size=5, stride=2)
+        self.conv3 = nn.Conv2d(36, 48, kernel_size=5, stride=2)
+        self.conv4 = nn.Conv2d(48, 64, kernel_size=3)
+        self.conv5 = nn.Conv2d(64, 64, kernel_size=3)
+        
+        self.fc1 = nn.Linear(64 * 1 * 18, 1164)  # Adjust based on image size
+        self.fc2 = nn.Linear(1164, 100)
+        self.fc3 = nn.Linear(100, 50)
+        self.fc4 = nn.Linear(50, 10)
+        self.fc5 = nn.Linear(10, 1)
+        
+        self.dropout = nn.Dropout(0.5)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.relu(self.conv4(x))
+        x = self.relu(self.conv5(x))
+        
+        x = x.view(x.size(0), -1)  # Flatten the tensor
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc3(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc4(x))
+        x = self.fc5(x)  # Output steering angle
+        return x
